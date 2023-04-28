9cb60487e56f0b49a7cb243a5bec6a3aa07800f2
SaidNivek <kevin.vi.dias@gmail.com>
2023-04-28 16:17:13 -0400

    start nav page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   # Defines the root path route ("/")
-  # root "articles#index"
+  # Change the homepage to setting a route to the root, and using the # to specify the page name
+  root 'pages#home'
 end
